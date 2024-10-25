56e978836f0f6c1a03a97f81f0d4e7ccf48e8e6e
Margo N <margoneff3@gmail.com>
2024-10-24 21:18:56 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+var openModal = function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+};
+
+button.addEventListener("click", function () {
+  if (button.classList.contains("got-this")) {
+    openModal();
+  }
+});
+
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  //console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+});
